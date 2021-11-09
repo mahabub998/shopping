9957fe2e55d911ce68c334508f27fa829ae8ab8b
mahabub alam <mahabu998@gmail.com>
2021-11-09 13:49:14 +0600

    some update

--- src/Components/Follow/Follow.js
+++ src/Components/Follow/Follow.js
@@ -9,22 +9,21 @@ const Follow = () => {
           <div className="col-md-4 d-flex ">
             <div>
               <img src="https://i.ibb.co/y8rY4wm/download.png" alt="" />
-              <p>lorem ipsum dolor sit</p>
+              <p className="ms-5 mt-2">lorem ipsum dolor sit</p>
             </div>
-
-            <h3 className="ms-5">Our blogs</h3>
+            <h3 className="ms-5"> blogs</h3>
           </div>
           <div className="col-md-4 d-flex">
             <div>
               <img src="https://i.ibb.co/ngJL29Q/instagarm.jpg" alt="" />
-              <p>lorem ipsum dolor sit</p>
+              <p className="ms-5 mt-2">lorem ipsum dolor sit</p>
             </div>
-            <h3 className="ms-2">Instagram</h3>
+            <h3 className="ms-2 ">Instagram</h3>
           </div>
           <div className="col-md-4 d-flex">
             <div>
               <img src="https://i.ibb.co/HYN1n6z/youtube.png" alt="" />
-              <p>lorem ipsum dolor sit</p>
+              <p className="ms-5 mt-2">lorem ipsum dolor sit</p>
             </div>
             <h3 className="ms-5">youtube</h3>
           </div>
